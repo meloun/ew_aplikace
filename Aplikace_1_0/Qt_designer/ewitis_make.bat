55e7be9048b9a0175e3d742ad3628a1df2094b54
meloun <lubos.melichar@gmail.com>
2013-05-31 20:47:27 +0200

    pyqt template

--- Aplikace_1_0/Qt_designer/ewitis_make.bat
+++ Aplikace_1_0/Qt_designer/ewitis_make.bat
@@ -3,6 +3,6 @@ IF "%1"=="" GOTO NE
 :JO
     pyuic4 -x mainwindow.ui -o %1
 :NE
-    pyuic4 -x ewitis_qt_template_v0_1.ui -o ewitis_qt_template_v0_1.py
+    pyuic4 -x ewitis_qt_template_v1_xx.ui -o ewitis_qt_template_v1_xx.py
     GOTO END   
 :END    
